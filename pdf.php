4ab8a80ea3a2be33cb423a5e8f3d372e50fadb87
zawye <zawye.pkm@gmail.com>
2019-03-21 20:33:21 +0630

    pdf

---
+++ pdf.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>pdf</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro" rel="stylesheet">
+</head>
+<body>
+    <div class="pdf">
+        <div class="frame">
+            <div class="name">
+                <h1>RAINBOW CINEMA</h1>
+                <hr>
+            </div>
+            <!-- section of name field -->
+            <div class="address">
+                <p>123 YOUR STREET</p>
+                <p>YOUR CITY, ST 12345</p>    
+                <p class="color">(123) 456-7890</p>
+                <p class="color">NO_REPLY@EXAMPLE.COM</p>
+            </div>
+            <!-- section of address -->
+            <div class="skill">
+                <h3>SKILL</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit omnis, sequi iusto dignissimos hic quidem aut molestiae commodi, ipsum saepe ipsam reprehenderit provident 
+                repudiandae recusandae laudantium natus voluptas minus ullam.</p>
+            </div>
+            <!-- skill field -->
+            <div class="experience">
+                <div class="extitle">
+                    <h3>EXPERIENCE</h3>
+                    <h4 class="color"> Company Name, Location <i class="i">—Job Title</i></h4>
+                    <span>MONTH 20XX - PRESENT</span>
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                        <li>Aenean ac interdum nisi. Sed in consequat mi.</li>
+                        <li>Sed in consequat mi, sed pulvinar lacinia felis eu finibus.</li>
+                    </ul>
+                </div>
+
+                <div class="extitle">
+                    <h4 class="color"> Company Name, Location <i class="i">—Job Title</i></h4>
+                    <span>MONTH 20XX - PRESENT</span>
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                        <li>Aenean ac interdum nisi. Sed in consequat mi.</li>
+                        <li>Sed in consequat mi, sed pulvinar lacinia felis eu finibus.</li>
+                    </ul>
+                </div>
+                <div class="extitle">
+                    <h4 class="color"> Company Name, Location <i class="i">—Job Title</i></h4>
+                    <span>MONTH 20XX - PRESENT</span>
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                        <li>Aenean ac interdum nisi. Sed in consequat mi.</li>
+                        <li>Sed in consequat mi, sed pulvinar lacinia felis eu finibus.</li>
+                    </ul>
+                </div>
+            </div>
+            <!-- end of the experience -->
+            <div class="education">
+                <h3>EDUCATION</h3>
+                <h4 class="color">School Name, Location  <i class="i">—Degree</i></h4>
+                <p>MONTH 20XX - MONTH 20XX, LOCATION</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                    Eligendi doloremque magnam enim, quia voluptatum assumenda vitae.
+                    praesentium perferendis sit facilis atque sint illo?</p>
+            </div>
+            <!-- end of the education -->
+            <div class="awards">
+                <h3>AWARDS</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                     Earum ut excepturi sequi porro aliquam in hic </p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
